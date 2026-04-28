d6f7c3e15433623ae1169d5b8e6f373a27b1e82f
Sammuel Victor <samvss13@gmail.com>
2026-04-28 23:06:02 +0000

    2009

--- demo/src/main/java/com/example/demo/controller/UserController.java
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -4,12 +4,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import com.example.demo.model.UserModel;
+import com.example.demo.service.UserService;
 
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
-    
+
+    UserService userService;
+    @GetMapping("/allUsers")
+    public List<UserModel> getAllUsers() {
+        return userService.getAll();
+    }
+
+
     @GetMapping("/hello")
     public String hello() {
         return "Hello, World!";
